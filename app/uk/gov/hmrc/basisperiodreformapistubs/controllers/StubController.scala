e72a972796da5963cf5ec4bd379313de621c1428
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2023-12-13 09:38:19 +0000

    APIS-6584 - Make the stubs just read files

---
+++ app/uk/gov/hmrc/basisperiodreformapistubs/controllers/StubController.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.basisperiodreformapistubs.controllers
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+import controllers.Assets
+
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents, Result}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+@Singleton()
+class StubController @Inject() (assets: Assets, cc: ControllerComponents)(implicit ec: ExecutionContext)
+    extends BackendController(cc) {
+
+  val errors: Map[(String, Option[String], Option[String]), Int] = List(
+    ("iv_overlap_relief_partnership", Some("987654321"), None) -> 400,
+    ("iv_overlap_sole_trader", Some("1000000A2"), None)        -> 400
+  ).toMap
+
+  def stub(path: String, utr: Option[String], part: Option[String]): Action[AnyContent] = Action.async { implicit request =>
+    val notFound =
+      s"""
+         |{
+         |  "name": "$path",
+         |  "elements": [],
+         |  "links": [
+         |    {
+         |      "rel": "self",
+         |      "href": "https://vdp.dev.denodo.hip.ns2n.corp.hmrc.gov.uk:9443/server/basis_period_reform/ws_overlap_relief_sole_trader/views/iv_overlap_relief_sole_trader?utr=100000002"
+         |    }
+         |  ]
+         |}
+         |
+         |""".stripMargin
+    val fullPath = s"/stubs/$path/"
+    val file     = s"utr${utr.getOrElse("")}_part${part.getOrElse("")}.json"
+    assets.at(fullPath, file)(request).map { result =>
+      if (result.header.status == 404) NotFound(Json.parse(notFound))
+      new Result(result.header.copy(status = errors.getOrElse((path, utr, part), 200)), result.body)
+    }
+  }
+}
---
+++ test/uk/gov/hmrc/basisperiodreformapistubs/controllers/StubControllerSpec.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.basisperiodreformapistubs.controllers
+
+import scala.concurrent.Future
+
+import org.scalatest.OptionValues
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.play.WsScalaTestClient
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+
+import play.api.http.Status
+import play.api.mvc.Result
+import play.api.test.Helpers._
+import play.api.test.{DefaultAwaitTimeout, FakeRequest, FutureAwaits}
+
+class StubControllerSpec extends AnyWordSpec with Matchers with OptionValues with WsScalaTestClient
+    with DefaultAwaitTimeout with FutureAwaits with GuiceOneAppPerSuite {
+  implicit def mat: org.apache.pekko.stream.Materializer = app.injector.instanceOf[org.apache.pekko.stream.Materializer]
+
+  "GET" should {
+    "return 200 and a file for sole trader" in {
+      val result = doGet("/iv_overlap_sole_trader?utr=100000002")
+      status(result) shouldBe Status.OK
+    }
+    "return 400 and a file for sole trader" in {
+      val result = doGet("/iv_overlap_sole_trader?utr=1000000A2")
+      status(result) shouldBe Status.BAD_REQUEST
+    }
+    "return 200 and a file for relief partnership" in {
+      val result = doGet("/iv_overlap_relief_partnership?utr=987654321&partnership_ref_number=5600000015")
+      status(result) shouldBe Status.OK
+    }
+    "return 400 and a file for relief partnership" in {
+      val result = doGet("/iv_overlap_relief_partnership?utr=987654321")
+      status(result) shouldBe Status.BAD_REQUEST
+    }
+
+    "return 200 and empty if not found" in {
+      val result = doGet("/iv_overlap_relief_partnership?utr=ABCDEFGH")
+      status(result) shouldBe Status.OK
+    }
+  }
+
+  def doGet(uri: String): Future[Result] = {
+    val fakeRequest = FakeRequest(GET, uri)
+    route(app, fakeRequest).get
+  }
+}
